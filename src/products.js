6b8b33e3f5d2a8b55b86d4124421710acc0db652
Alejandro Rendón <jalgrisales@poligran.edu.co>
2024-12-03 19:41:21 -0500

    Creacion modulo carrito de compras

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "New Balance 9060",
+    price: 560000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "New Balance 2002R",
+    price: 420000,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Jordan 3 Retro OG",
+    price: 1280000,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Nike Kobe 8 Protro",
+    price: 320000,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Nike Vapor Edge Kobe 6 SE",
+    price: 950000,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Nike Air Force 1 Low",
+    price: 620000,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Jordan 4 Retro",
+    price: 830000,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Nike Air Force 1 Low '07",
+    price: 750000,
+    productImage: product8,
+  },
+];
